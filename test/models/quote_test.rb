6ba18d79028e417fa4565f0898e76cf0b0519603
Devin Mois <dwmois7@gmail.com>
2016-01-05 19:15:12 +0000

    add FactoryGirl and update tests

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
   test "unique_tag" do
-    quote = Quote.create(:author => 'Devin Mois', :saying => 'You only yolo once.')
+    quote = FactoryGirl.create(:quote, :author => 'Devin Mois')
     expected = 'DM#' + quote.id.to_s
     actual = quote.unique_tag
     assert_equal expected, actual
